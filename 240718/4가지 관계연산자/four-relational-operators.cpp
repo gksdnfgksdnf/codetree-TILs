4bc7665762d9f9321b185857eb6c36f35853075e
Hanul <123638927+gksdnfgksdnf@users.noreply.github.com>
2024-07-18 01:02:24 +0900

    Title: 4가지 관계연산자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240718/4가지 관계연산자/four-relational-operators.cpp
+++ 240718/4가지 관계연산자/four-relational-operators.cpp
@@ -4,8 +4,8 @@ using namespace std;
 int main() {
     int a,b;
     cin>>a>>b;
-    cout<<a>=b<<"\n";
-    cout<<a>b<<"\n";
-    cout<<a<=b<<"\n";
-    cout<<a<b;
+    cout<<(a>=b)<<"\n";
+    cout<<(a>b)<<"\n";
+    cout<<(a<=b)<<"\n";
+    cout<<(a<b);
 }
