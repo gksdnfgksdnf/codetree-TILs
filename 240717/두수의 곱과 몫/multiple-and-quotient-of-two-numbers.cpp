ae36807a7d8dd2fbf74f74428cc2a8a90bd4b7fc
Hanul <123638927+gksdnfgksdnf@users.noreply.github.com>
2024-07-17 23:47:59 +0900

    Title: 두수의 곱과 몫, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

--- 240717/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.cpp
+++ 240717/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.cpp
@@ -4,6 +4,6 @@ using namespace std;
 int main() {
     int a,b;
     cin>>a>>b;
-    printf("%d * %d = %d", a, b, a * b);
+    printf("%d * %d = %d\n", a, b, a * b);
     printf("%d / %d = %d", a, b, a / b);
 }
