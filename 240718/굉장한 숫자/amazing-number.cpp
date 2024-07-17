41b1b2b5f4b28dc7e2642e577b681c7ba6fa7b66
Hanul <123638927+gksdnfgksdnf@users.noreply.github.com>
2024-07-18 01:30:51 +0900

    Title: 굉장한 숫자, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240718/굉장한 숫자/amazing-number.cpp
+++ 240718/굉장한 숫자/amazing-number.cpp
@@ -4,5 +4,5 @@ using namespace std;
 int main() {
     int n;
     cin>>n;
-    cout<<(n%2==0?:(n%5==0?:"true":"false"):n%3==0?n%5==0?"true":"false":"false");
+    cout<<(n%2==0?n%5==0?:"true":"false":n%3==0?n%5==0?"true":"false":"false");
 }
