05f902f25fe2e4a1f91d3eb7e2c56eeb6b8fd748
Hanul <123638927+gksdnfgksdnf@users.noreply.github.com>
2024-07-18 01:03:55 +0900

    Title: 4번의 크기 비교, Time: 19ms, Memory: 0MB, Status: Passed - CodeTree

--- 240718/4번의 크기 비교/4-time-comparison.cpp
+++ 240718/4번의 크기 비교/4-time-comparison.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 int main() {
     int a,b,c,d,e;
-    cin>>a>>b>c>>d>>e;
+    cin>>a>>b>>c>>d>>e;
     cout<<(a>b)<<"\n";
     cout<<(a>c)<<"\n";
     cout<<(a>d)<<"\n";
-    cout<<(a>e)<<"\n";
+    cout<<(a>e);
 }
