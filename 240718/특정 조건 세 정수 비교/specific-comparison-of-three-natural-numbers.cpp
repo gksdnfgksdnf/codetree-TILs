4cc84cedbd093b50d86a2b111c06c46b739b22f8
Hanul <123638927+gksdnfgksdnf@users.noreply.github.com>
2024-07-18 01:21:30 +0900

    Title: 특정 조건 세 정수 비교, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240718/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int a,b,c;
+    cin>>a>>b>>c;
+    if(a==min(min(a,b)c))
+        cout<<1;
+    else
+        cout<<0;
+    if(a==b&&b==c)
+        cout<<1;
+    else
+        cout<<0;
+}
