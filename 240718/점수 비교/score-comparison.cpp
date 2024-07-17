2e01b551560ecdefce75c4ae21fb99510de4bef2
Hanul <123638927+gksdnfgksdnf@users.noreply.github.com>
2024-07-18 01:18:23 +0900

    Title: 점수 비교, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240718/점수 비교/score-comparison.cpp
+++ 240718/점수 비교/score-comparison.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-    int a1,a2,b1,b2;
-    cin>>a1>>a2>>b1>>b2;
-    cout<<(a1>b1?a2>b2?1:0:0);
+    int a,b,c,d;
+    cin>>a>>b>>c>>d;
+    cout<<(a>c?b>d?1:0:0);
 }
